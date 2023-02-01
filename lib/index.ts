46c5597f0eae73646a306636b2b26f00da6677e4
邱子建 <qiuzijian@qiuzijiandeMacBook-Air.local>
2023-02-01 15:44:44 +0800

    feat: 新增获取场景数据

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,9 @@
-console.log("测试执行");
+import fetch from 'node-fetch';
+
+const getScene = async () => {
+  const response = await fetch("https://hub.realibox.com/api/hub/v1/studio/scenes/publish/2371408606185652314");
+  const res = await response.json();
+  return res
+}
+
+getScene();
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['lib/index.ts'],
 
-  format: ["cjs"],
+  format: ["esm"],
 
   dts: false,
 
